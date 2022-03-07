b1dc865e494a5a6cea1e218de32cb86e81ae127b
Daniel Sapkota <daniel@waterbot.xyz>
2022-03-06 18:51:07 -0800

    Add the response format

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require("express")
+var app = express()
+var fs = require('fs');
+
+var HTTP_PORT = 8000
+
+app.listen(HTTP_PORT, () => {
+    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+});
+
+app.get("/", (req, res, next) => {
+    res.json({"message":"Ok"})
+});
+
+app.get("/matches", (req, res, next) => {
+    var obj = JSON.parse(fs.readFileSync('odds.json', 'utf8'));
+    res.json(obj)
+});
+
+
+app.use(function(req, res){
+    res.status(404);
+});
