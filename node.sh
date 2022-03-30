89a738ca29d3bb335e65e5b1e071e2415f2f7263
Daniel Sapkota <daniel@waterbot.xyz>
2022-03-30 15:28:29 -0700

    Use localhost ganache

---
+++ node.sh
@@ -0,0 +1,1 @@
+ganache-cli --fork https://eth-mainnet.alchemyapi.io/v2/zzdikT2BoUCO8gxk57lf8_NqFQ0MnUG7 --unlock 0x2faf487a4414fe77e2327f0bf4ae2a264a776ad2
--- test/test.js
+++ test/test.js
@@ -1,21 +1,35 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
-const { DAI, WHALE, erc20ABI } = require("./config")
-const IERC20 = artifacts.require("IERC20")
+const { DAI_ADDY, WHALE_ADDY, erc20ABI } = require("./config")
 
-let daiErc20Contract = new ethers.Contract(DAI,erc20ABI);
+let daiContract = new ethers.Contract(DAI_ADDY,erc20ABI);
+
+
 
 it("Token Transfer", async function () {
+    const FUND_AMOUNT = (BigInt(2)*BigInt(10**16)).toString()
+
     const [owner, addr1, addr2, addr3] = await ethers.getSigners();
+
     const Liquidity = await ethers.getContractFactory("Liquidity");
-    const liq = await Liquidity.deploy(DAI);
-
-
-    await liq.deployed();
-
+    const liq = await Liquidity.deploy(DAI_ADDY);
+    await liq.deployed();  
     
+    console.log("Contract Deployerd at " + liq.address);
 
     // await liq.addLiquidity("");
-    const whale = await ethers.provider.getSigner(WHALE)
+    const whale_signer = await ethers.provider.getSigner(WHALE_ADDY);
+
+    let DAI = await ethers.getContractAt(erc20ABI, DAI_ADDY, whale_signer);
+    let balance = await DAI.balanceOf(WHALE_ADDY)  / 1e18;
+
+    await DAI.transfer(liq.address, FUND_AMOUNT, {
+            from: WHALE_ADDY,
+          });
+
+    console.log("Whale Balance: " + balance)
+    // await DAI.mint(owner, ethers.utils.parseEther('10000'))
+
+    // await
 
 })
