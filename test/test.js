71061be8bb464cf77a88877078cf6585cee0a8df
Daniel Sapkota <daniel@waterbot.xyz>
2022-03-30 14:06:17 -0700

    Writing the test

--- test/test.js
+++ test/test.js
@@ -1,11 +1,21 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
+const { DAI, WHALE, erc20ABI } = require("./config")
+const IERC20 = artifacts.require("IERC20")
 
-const DAI = "0x773616e4d11a78f511299002da57a0a94577f1f4";
+let daiErc20Contract = new ethers.Contract(DAI,erc20ABI);
 
-it("Basic", async function () {
+it("Token Transfer", async function () {
     const [owner, addr1, addr2, addr3] = await ethers.getSigners();
     const Liquidity = await ethers.getContractFactory("Liquidity");
     const liq = await Liquidity.deploy(DAI);
+
+
     await liq.deployed();
+
+    
+
+    // await liq.addLiquidity("");
+    const whale = await ethers.provider.getSigner(WHALE)
+
 })
