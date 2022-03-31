4dcc8f2e64584f37d615648b6e248b619738cbf1
June Liang <liangj10@unlv.nevada.edu>
2022-03-30 21:16:29 -0700

    feature page swiper with links now

--- src/components/Dashboard/CustomSwiper.jsx
+++ src/components/Dashboard/CustomSwiper.jsx
@@ -9,8 +9,14 @@ import "swiper/css/navigation";
 import { Navigation, Mousewheel, Keyboard } from "swiper";
 
 import Image from 'next/image'
+import { useRouter } from "next/router";
+
+const swiperImageNames = ['Esport_Betting.jpg',"Online_Betting.jpg",'NBA_Betting.jpg','EPL_Betting.jpg','Esport_Betting_2.jpeg'];
+const swiperImageUrls = ['#','#','/Matches/Basketball/NBA','Matches/Soccer/EPL','#']
 
 export const CustomSwiper = () => {
+  const router = useRouter();
+
   const [slideCount,setSlideCount] = useState(1);
 
   const handleResize = () =>{
@@ -26,8 +32,7 @@ export const CustomSwiper = () => {
       <Swiper
         cssMode={true}
         navigation={true}
-        mousewheel={true}
-        modules={[Navigation, Mousewheel, Keyboard]}
+        modules={[Navigation]}
         className="feature-page-pictures-swiper"
         style={{paddingTop:'0px',width:'95%'}}
         slidesPerView={slideCount}
@@ -35,8 +40,8 @@ export const CustomSwiper = () => {
         loop={true}
       >
       
-        {['Esport_Betting.jpg',"Online_Betting.jpg",'NBA_Betting.jpg','EPL_Betting.jpg','Esport_Betting_2.jpeg'].map((item,index)=>{
-          return(<SwiperSlide key={index} style={{borderRadius:"30px",overflow:'auto',width:'770px',height:'350px',cursor:'pointer'}} ><Image src={`/static/images/featured_page_pictures/${item}`} layout='fill'/></SwiperSlide>)
+        {swiperImageNames.map((item,index)=>{
+          return(<SwiperSlide key={index} style={{borderRadius:"30px",overflow:'auto',width:'770px',height:'350px',cursor:'pointer'}} onClick={()=>router.push(swiperImageUrls[index])}><Image src={`/static/images/featured_page_pictures/${item}`} layout='fill'/></SwiperSlide>)
         })}
         
 
