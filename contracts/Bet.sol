1ab231c547105a5f0ecc314285c341ce58da0760
Daniel Sapkota <daniel@waterbot.xyz>
2022-04-10 18:09:51 -0700

    Resolving Markets

--- contracts/Bet.sol
+++ contracts/Bet.sol
@@ -8,8 +8,8 @@ contract Bet is ERC1155{
         uint256 timestamp; //unix timestamp of when the bet was made
         address operator;  // users address
         uint80 gameId; // the ID of the game with which this token is connected
-        int8 winnerIndex; //0,1,2,3. What outcome the user chose
-        uint128 bet;         // the amount gambled by the user
+        int8 betIndex; //0,1,2,3. What outcome the user chose
+        uint128 bet_amount;         // the amount gambled by the user
         uint128 to_win; //the amount user can win
         uint8 status; //0 means pending, 1 means settled, 2 means canceled
     }
@@ -26,23 +26,21 @@ contract Bet is ERC1155{
        DAI = _DAI;
     }
 
-    function createBet(uint256 timestamp, uint80 gameId, int8 winnerIndex, uint128 bet) public {
+    function createBet(uint80 gameId, int8 betIndex, uint128 bet_amount) public {
 
-        (bool success, bytes memory data) = DAI.call(abi.encodeWithSelector(0x23b872dd, msg.sender, this, bet));
-
-        
+        (bool success, bytes memory data) = DAI.call(abi.encodeWithSelector(0x23b872dd, msg.sender, this, bet_amount));
 
         if (success){
 
             uint176 currId = _nextId+1;
 
             _bets[currId] = singleBet({
-                timestamp: timestamp,
+                timestamp: block.timestamp,
                 operator: msg.sender,
                 gameId: gameId,
-                winnerIndex: winnerIndex,
-                bet: bet,
-                to_win: bet, //get this from odds
+                betIndex: betIndex,
+                bet_amount: bet_amount,
+                to_win: bet_amount, //get this from odds
                 status: 0
              });
 
@@ -54,12 +52,12 @@ contract Bet is ERC1155{
 
     function withdrawBet(uint176 tokenId) public {
         singleBet storage curr_bet = _bets[tokenId];
+
+        //also check that the market has been resolved
         require(curr_bet.status == 1 && curr_bet.to_win > 0, 'Not cleared');
 
         delete _bets[tokenId]; 
 
         _burn(msg.sender, tokenId, 1);
     }
-
-
 }
--- contracts/OpenBook.sol
+++ contracts/OpenBook.sol
@@ -2,65 +2,61 @@ pragma solidity ^0.8.0;
 
 //Look at how uniswap handles this
 contract OpenBook{
+    event MarketCreated(uint256 id, string[] names, uint256[] initialOdds);
+
+    uint256 public protocolFee;
+    uint256 public LPFee;
+
+    struct Market {
+        string[] names;
+        int8 winnerIndex;
+        uint256 protocolFee;
+        uint256 LPFee;
+        uint256 creationTimestamp;
+        uint256 resolutionTimestamp; // when winner is declared
+        uint256[] odds;
+        bool active; // false if not ready to use or if resolved
+    }
+
+    Market[] internal markets;
 
 
-
-   constructor() public{
+   constructor(uint256 protocolFee, uint256 LPFee) public{
+       protocolFee = protocolFee;
+        LPFee = LPFee;
     }
 
 
     //Creates new markets. Resolve current markets
     function updateMarkets() public{
-
+        
     }
 
+    //Update odds
     function updateOdds() public{
-
+        markets[1].odds = [1,2,3];
     }
 
     function getAddress() public view returns (address){
         return msg.sender;
     }
 
+    function startMarket(string[] memory _names, uint256[] memory _initialOdds, uint256 resolutionTimestamp) internal returns (uint256 _marketId) {
+        _marketId = markets.length;
 
-    // uint256 _winningShares = _market.winner.trustedBurnAll(msg.sender);
-    // _winningShares = (_winningShares / shareFactor) * shareFactor; // remove unusable dust
+        markets.push(
+            Market(
+                _names,
+                -1,
+                protocolFee,
+                LPFee,
+                block.timestamp,
+                resolutionTimestamp,
+                _initialOdds,
+                false
+            )
+        );
 
-
-
-    // Market memory _market = markets[_id];
-    //     for (uint256 _i = 0; _i < _market.shareTokens.length; _i++) {
-    //         _market.shareTokens[_i].trustedMint(_receiver, _shareToMint);
-    //     }
-
-    // function startMarket(
-    //     address _settlementAddress,
-    //     string[] memory _names,
-    //     uint256[] memory _initialOdds,
-    //     bool _active
-    // ) internal returns (uint256 _marketId) {
-    //     _marketId = markets.length;
-    //     markets.push(
-    //         Market(
-    //             _settlementAddress,
-    //             createShareTokens(_names, address(this)),
-    //             OwnedERC20(0),
-    //             0,
-    //             settlementFee,
-    //             protocolFee,
-    //             stakerFee,
-    //             block.timestamp,
-    //             0,
-    //             _initialOdds,
-    //             _active
-    //         )
-    //     );
-    //     emit MarketCreated(_marketId, _names, _initialOdds);
-    //     if (_active) {
-    //         emit MarketActivated(_marketId);
-    //     }
-    // }
-
-
-
+        emit MarketCreated(_marketId, _names, _initialOdds);
+    }
 }
