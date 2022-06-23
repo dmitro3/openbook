e51dfb2b7f525721b669e6553f8daaa1df7ff359
Daniel Sapkota <daniel@waterbot.xyz>
2022-06-23 16:34:05 -0700

    Fucking finally solving the multiple LP model

--- contracts/Liquidity.sol
+++ contracts/Liquidity.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "./interfaces/IBet.sol";
+import "./interfaces/IERC20.sol";
 
 contract Liquidity is ERC1155{
 
@@ -10,6 +11,8 @@ contract Liquidity is ERC1155{
     address public DAI;
     address public BET_CONTRACT;
     uint32 public val_set = 0;
+    uint32 public totalSupply = 0;
+
     
     modifier onlyBet{
         require(msg.sender == BET_CONTRACT);
@@ -26,24 +29,56 @@ contract Liquidity is ERC1155{
         val_set = 1;
     }
 
+    function getLockedLiquidity()  public returns (uint256) {
+        return IBet(BET_CONTRACT).getLockedLiquidity();
+    }
+
+    function getDAIBalance() public returns (uint256) {
+        return IERC20(DAI).balanceOf(this);
+    }
+
+    function getFreeFunds() public returns (uint256) {
+        return (getDAIBalance() - getLockedLiquidity());
+    }
+
+    function getShareValue(uint256 _amount) returns (uint256) {
+        if (totalSupply == 0){
+            return _amount;
+        }
+
+        return _amount * getFreeFunds() / totalSupply;
+    }
+
     function sendWithdrawl(address _receipent, uint256 _amount) public onlyBet returns (bool) {
         (bool success, bytes memory data) = DAI.call(abi.encodeWithSelector(0x23b872dd, this, _receipent, _amount));
         return success;
     }
 
+
     function addLiquidity(uint256 _amount)  public {
         (bool success, bytes memory data) = DAI.call(abi.encodeWithSelector(0x23b872dd, msg.sender, this, _amount));
 
         if (success)
-            _mint(msg.sender, LIQUIDITY, _amount, "");
+        {
+            if (totalSupply > 0) {
+                shares =  _amount * (totalSupply / getFreeFunds());
+            }
+            else {
+                shares = _amount;
+            }
+
+         
+            _mint(msg.sender, LIQUIDITY, shares, "");
+            total_shares = total_shares + shares;
+
+        }
     }
 
     function removeLiquidity(uint256 _amount)  public {
 
         uint256 balance = this.balanceOf(msg.sender, LIQUIDITY);
-        uint256 lockedLiquidity = IBet(BET_CONTRACT).getLockedLiquidity();
-
-        require(balance - lockedLiquidity > _amount, "balance - lockedLiquidity must be greater than requested amt");
+        
+        require(_shareValue(_amount) >= _amount, "User's capital must be greater than requested amt");
 
         (bool success, bytes memory data) = DAI.call(abi.encodeWithSelector(0x23b872dd, this, msg.sender, _amount));
 
