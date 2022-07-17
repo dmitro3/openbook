e2a87eaed703c05fd2ab878ce923fd5b05420a37
Daniel Sapkota <daniel@waterbot.xyz>
2022-07-17 13:31:21 -0700

    Setting the odds

--- scripts/provider.js
+++ scripts/provider.js
@@ -1,71 +1,82 @@
-const { ethers } = require("hardhat");
 const axios = require('axios')
-const {MARKETS_ADDY} = require("../src/config")
-
-
-// get all markets and set odds
+const {VAULTMANAGER_ADDY, VAULTMANAGER_ABI, VAULT_ABI, MARKETS_ADDY, MARKETS_ABI, HTTP_PROVIDER} = require("../src/config")
+const Web3 = require("web3")
 
 async function update_odds(){
+    const web3 = new Web3(HTTP_PROVIDER);
+    const account = web3.eth.accounts.privateKeyToAccount("0x" + process.env.ETH_KEY);
 
-    sports = [
-        {'key': 'americanfootball_ncaaf', 'group': 'American Football', 'title': 'NCAAF'},
-        {'key': 'americanfootball_nfl', 'group': 'American Football', 'title': 'NFL'},
-        {'key': 'soccer_usa_mls', 'group': 'Soccer', 'title': 'MLS'},
-        {'key': 'soccer_epl', 'group': 'Soccer', 'title': 'EPL'},
-        {'key': 'soccer_spain_la_liga', 'group': 'Soccer', 'title': 'La Liga - Spain'},
-        {'key': 'soccer_italy_serie_a', 'group': 'Soccer', 'title': 'Serie A - Italy'},
-        {'key': 'soccer_fifa_world_cup', 'group': 'Soccer', 'title': 'FIFA World Cup'},
-    ]
-
-    //Set odds in an easy to access dictionary
-    for (const row of sports){
-        let res2 = await axios.get(`https://api.the-odds-api.com/v4/sports/${row['key']}/odds?regions=uk&markets=h2h&apiKey=${process.env.ODDS_API}&daysFrom=1`);
+    //manually set to your vault
+    let contract = new web3.eth.Contract(VAULTMANAGER_ABI, VAULTMANAGER_ADDY);
+    let vaults = await contract.methods.getAllVaults().call()
+    let vault = vaults[0]
 
 
-        for (const event of res2.data){
+    let market_contract = new web3.eth.Contract(MARKETS_ABI, MARKETS_ADDY);
+    let matches = await market_contract.methods.getAllMarkets().call()
 
-            let odds = {};
+    let match_details = []
+    
+    for (const match of matches) {
+        let curr_details = await market_contract.methods.marketDetailsById(match).call()
 
-            for (let outcomes in event['bookmakers']){
-                for (let outcome in event['bookmakers'][outcomes]['markets'][0]['outcomes']){
-
-                    let sel = event['bookmakers'][outcomes]['markets'][0]['outcomes'][outcome]
-
-                    if (sel['name'] == event['home_team'])
-                        odds["1"] = sel['price']
-
-                    if (sel['name'] == event['away_team'])
-                        odds["2"] = sel['price']
-
-                    if (sel['name'] == "Draw")
-                        odds["X"] = sel['price']
-
-                }
-            }
-            console.log(odds)
-
-            if (process.env.HARDHAT_NETWORK == 'localhost'){
-                break;
-            }
+        if (curr_details['active'] == true){
+            match_details[match] = curr_details
         }
-
-
-
     }
 
-    // const MyContract = await ethers.getContractFactory("Markets");
-    // const markets = await MyContract.attach(MARKETS_ADDY);
+    console.log(match_details)
 
-    // let matches = await markets.getAllMarkets()
+    // let vault_contract = new web3.eth.Contract(VAULT_ABI, vault);
+    // console.log(vault)
+    
+    // sports = [
+    //     {'key': 'americanfootball_ncaaf', 'group': 'American Football', 'title': 'NCAAF'},
+    //     {'key': 'americanfootball_nfl', 'group': 'American Football', 'title': 'NFL'},
+    //     {'key': 'soccer_usa_mls', 'group': 'Soccer', 'title': 'MLS'},
+    //     {'key': 'soccer_epl', 'group': 'Soccer', 'title': 'EPL'},
+    //     {'key': 'soccer_spain_la_liga', 'group': 'Soccer', 'title': 'La Liga - Spain'},
+    //     {'key': 'soccer_italy_serie_a', 'group': 'Soccer', 'title': 'Serie A - Italy'},
+    //     {'key': 'soccer_fifa_world_cup', 'group': 'Soccer', 'title': 'FIFA World Cup'},
+    // ]
 
-    // for (const match of matches) {
-    //     let match_detail = await markets.marketDetailsById(match)
+    // let odds = {}
 
-    //     console.log(match_detail['id'], match_detail['matchTimestamp'])
+    // //Set odds in an easy to access dictionary
+    // for (const row of sports){
+    //     let res2 = await axios.get(`https://api.the-odds-api.com/v4/sports/${row['key']}/odds?regions=uk&markets=h2h&apiKey=${process.env.ODDS_API}&daysFrom=1`);
 
-    //     if (match_detail['active'] == true){
 
+    //     for (const event of res2.data){
+
+    //         let odds = {};
+
+    //         for (let outcomes in event['bookmakers']){
+    //             for (let outcome in event['bookmakers'][outcomes]['markets'][0]['outcomes']){
+
+    //                 let sel = event['bookmakers'][outcomes]['markets'][0]['outcomes'][outcome]
+
+    //                 if (sel['name'] == event['home_team'])
+    //                     odds["1"] = sel['price']
+
+    //                 if (sel['name'] == event['away_team'])
+    //                     odds["2"] = sel['price']
+
+    //                 if (sel['name'] == "Draw")
+    //                     odds["X"] = sel['price']
+
+    //             }
+    //         }
+
+    //         console.log(event['home_team'], event['away_team'],  odds)
+
+    //         if (process.env.HARDHAT_NETWORK == 'localhost'){
+    //             break;
+    //         }
     //     }
+
+
+
     // }
 }
 
